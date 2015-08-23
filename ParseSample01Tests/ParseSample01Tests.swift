e0720b973087d145313a268d696a1730c842322b
Kazuya Tamakoshi <kazuya.tamakoshi+git@franliber.co.jp>
2015-08-24 01:13:29 +0900

    Initial Commit

---
+++ ParseSample01Tests/ParseSample01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ParseSample01Tests.swift
+//  ParseSample01Tests
+//
+//  Created by k on 2015/08/24.
+//  Copyright (c) 2015年 FranLiber Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ParseSample01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
